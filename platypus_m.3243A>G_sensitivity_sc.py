46e730c11e1cc756f7bcabcb13dd244caca3392e
Stuart Cannon <sc651@grace.ex.ac.uk>
2015-10-12 15:50:06 +0100

    started bilding if statee=metns to construct filepaths to different locations dependent on the sample number

--- platypus_m.3243A>G_sensitivity_sc.py
+++ platypus_m.3243A>G_sensitivity_sc.py
@@ -1,6 +1,5 @@
 #import the relevant modules
-import subprocess #this will be used to get the operating system (Linux Scientific) to execute a command
-import glob
+import glob, re, subprocess
 
 #assign the following variables:
 #path to the platypus variant caller
@@ -9,53 +8,83 @@ path_to_platypus  = "usr/share/platypus/Platypus_0.8.1/"
 #path to the reference sequence
 build_37_ref  = "/mnt/Data1/resources/human_glk_u37.fasta" 
 
-#path to bam files without specific sample
+#path to bam file directories
 path_to_P5_bam = "/mnt/Data1/targeted_sequencing/"
 path_to_V5_bam  = "/mnt/Data4/targeted_sequencing/" #this also includes the old notation in the format P5_xyz where xyz are itegers
 
-#interval file for the m.3243A>G mutation with 25bp either side 
+# assigning variable to be called by the command line with platypus.  m.3243A>G mutation with 25bp either side 
 #because only looking at a single region there is no need to create an interval list
 interval_mtDNA = "chrM:3209-3249"
 
 #base path to where the script is being run from and the files are being saved/output
 base_path = "/mnt/Data4/working_directory/stuart/python-2-7-10/scripts/platypus"
 
-#Initially using the manually created subset of samples just from the MODY panel
+#createing regexs for different combinations of bam filename
+format_p = re.compile(single character. the letter 5. under score. 5. underscore. three letter number)
+format_v = re.compile()
 
 #Constructiong a dictionary of sample number as key and the path to appropriate bam file as the value
 for line in MODY_subset_infile:
 	line.split()
-	use regular expression to infer the location of the bam file based on the column 1 must meet format of "P5_5_211" because P5's above 500 are stroed in data 4 
+	use regular expression to infer the location of the bam file based on the column 0. must meet format of "P5_5_211" because P5's above 500 are stroed in data 4 
 	#if sample corresponds with old version numbers. 
-	if column 0 has a 'p' in it:
-		assign column 0 to variable 'sample_ID'
-		extract the numerical sample number values by taking the last three digits, turn to an integer and assign to 'p_numeric_sample_ID'
-		if P_numeric_sample_ID is between 1 and 48:
+	if format_p(column 0) == True: #is in foramt P5_5_221
+		sample_ID = column(0)
+		p_numeric_sample_ID = int(sample_ID[-3:])#uses reverse indicies extract the numerical sample number values by taking the last three digits, turn to an integer and assign to 'p_numeric_sample_ID'
+		bam_end = '.realigned.sorted.bam'
+
+		if p_numeric_sample_ID >= 1 and <= 48:
 			additional_ path = 'P5_001-048/assembly/'
-			bam_file_name = 'P5_001-48_' + str(p_numeric_sample_ID) + '.realigned.sorted.bam'
+			bam_file_name = 'P5_001-48_' + str(p_numeric_sample_ID) + bam_end
 			#append dictionary to include sample_ID as key and string of path to bam file
-			dictionary key == 'sample_ID', dictionary value == path_to_P5_bam + additional path + bam_file_name 
-		elif sample number is between  49 and 96:
-			additional_path = 
-			bam_file_name = same format as above 
-			dictionary_key == 
-		elif sample number is between 97 and 144:
-			same as above
-		elif sample number is between 145 and 216:
-			same as above
-		elif sample number is between 217 and 240:
-			same as above
-		elif sample number is between 241 and 336:
-			same as above
-		elif sample number is between 337 and 384:
-			same as above
-		elif sample number is between 385 and 432: 
-			need to use file extension realigned.sorted.merged.bam
-			additional path = P5_385-432_run2
-		elif sample number is between 433 and 480:
-			same as first one
-		elif sample number is between 481 and 504:
-			same as first one	
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_fie_name
+
+		elif p_numeric_sample_ID >=  49 and <= 96:
+			additional_path = 'P5_049_096/assembly/' 
+			bam_file_name = 'P5_049-096_' + str(p_numeric_sample_ID) + bam_end
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_file_name 
+
+		elif p_numeric_sample_ID >= 97 and <= 144:
+			additional_path = 'P5_097-144/assembly/'
+			bam_file_name = 'P5_049-144_' + str(p_numeric_sample_ID) + bam_end
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_file_name
+		
+		elif p_numeric_sample_ID >= 145 and <= 216:
+			additional_path = 'P5_145-216/assembly/'
+			bam_file_name = 'P5_145-216_' + str(p_numeric_sample_ID) + bam_end
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_file_name
+
+		elif p_numeric_sample_ID >= 217 and <= 240:
+			additional_path = 'P5_217-240/assembly/'
+			bam_file_name = 'P5_217-240_' + str(p_numeric_sample_ID) + bam_end
+			my_dict[sample_ID] = path_to_P5_bam +additional_path + bam_file_name
+
+		elif p_numeric_sample_ID >= 241 and <= 336:
+			additional_path = 'P5_241-336/assembly/'
+			bam_file_name = 'P5_241-336_' + str(p_numeric_sample_ID) + bam_end
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_file_name
+
+		elif p_numeric_sample_ID >= 337 and <= 384:
+			additional_path = 'P5_337-384/assembly/'
+			bam_file_name = 'P5_337-384_' + str(p_numeric_sample_ID) + bam_end
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_file_name
+
+		elif p_numeric_sample_ID >= 385 and 432: 
+			additional_path = 'P5_385-432_run2/assembly/'
+			bam_file_name = 'P5_385-432_' + str(p_numeric_sample_ID) + 'realigned.sorted.merged.bam'
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_file_name
+
+		elif p_numeric_sample_ID >= 433 and <= 480:
+			additional_path = 'P5_433-480/assembly/'
+			bam_file_name = 'P5_433-480_ + str(p_numeric_sample_ID) + bam_end
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_file_name
+			
+
+		elif p_numeric_sample_ID >= 481 and <= 504:
+			additonal_path = 'P5_481-504/assembly/'
+			bam_file_name = 'P5_481-504_' + str(p_numeric_sample_ID) + bam_end
+			my_dict[sample_ID] = path_to_P5_bam + additional_path + bam_file_name
+	
 	elif column 0 meets the regular expression in the format of P5_505 or has a v5 in it:
 		sample_ID = str(column0 last three digits) 
 		take column 1 and turn into a string
@@ -75,12 +104,7 @@ for line in MODY_subset_infile:
 	else:
 		print 'error with input file'
 
-
-
-
 #loop through each dictionary entry and initiate Platypus calls on it
-
-
 
 #create a vcf output file path consisting of: 
 vcf_output = base_path + "/variants/" + sample_id + "platypus_m.3243A>G.vcf"
