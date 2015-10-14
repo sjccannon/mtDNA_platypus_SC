eb3c541bff4cb41dd06d02f3d0360e8205f71250
Stuart Cannon <sc651@grace.ex.ac.uk>
2015-10-14 15:30:46 +0100

    tested and de bugged second section

--- platypus_m.3243A>G_sensitivity_sc.py
+++ platypus_m.3243A>G_sensitivity_sc.py
@@ -1,5 +1,5 @@
 #import the relevant modules
-import glob, re, subprocess
+import glob, re, subprocess, os, fnmatch
 
 #assign the following variables:
 #path to the platypus variant caller
@@ -21,7 +21,7 @@ base_path = "/mnt/Data4/working_directory/stuart/python-2-7-10/scripts/platypus"
 
 #createing regexs for different combinations of bam filename
 format_P5 = re.compile('P5_\d_\d{1,3}')#matches samples in format P5_n_nnn
-format_PorV = re.IGNORECASE('[Pv]\d+_\d+}') #includes samples in format v5_nnn, v501_nnnn, P5_nnn
+format_PorV = re.compile('[Pv]\d+_\d+') #includes samples in format v5_nnn, v501_nnnn, P5_nnn
 
 #creating a dictionary look up
 my_dict = {}
@@ -100,68 +100,75 @@ with open("test.csv", 'r') as in_file:
 				my_dict[sample_ID] = path_to_P5_bam + extra_path + bam_file_name
 
 			else:
-				print 'error'
+				print 'error' + str(ID_num)
+
+
 
 #print my_dict
 
-#maybe redundant
-#ascertaining how many digits the sample number contains so we can later extract the numerics from sample number from the 
-#ID_num_3 = re.compile('*_\d{3}') 
-#ID_num_4 = re.compile('*_\d{4}') 
-
-def rearrange_bam_location(path_to_bam_directory):
-	for file in os.listdir(path_to_bam_directory):
-		bam_pattern = sample_ID + '.realigned.sorted.bam'
-		if fnmatch.fnmatch(file, bam_pattern):
-			complete_path_to_bam = path_to_bam_directory + file
-			my_dict[sample_ID] = complete_path_to_bam
-	return my_dict
+#def rearrange_bam_location(path_to_bam_directory):
+#	for file in os.listdir(path_to_bam_directory):
+#		bam_pattern = sample_ID + '.realigned.sorted.bam'
+#	 	print 'function' + bam_pattern	
+#		if fnmatch.fnmatch(file, bam_pattern):
+#			complete_path_to_bam = path_to_bam_directory + file
+#			print complete_path_to_bam
+#			my_dict[sample_ID] = complete_path_to_bam
+#	return my_dict
 			
-	 
-
 #if the sample_ID matches the later panels this code finds the file location in Data4 based on the batch number and sample number and the deduced naming ocnventions within those files 
 		elif format_PorV.match(columns[0]):
+			#print columns[0]
 			sample_ID = str(columns[0])
+			#print sample_ID
+			file_names_with_sex = sample_ID + "*.realigned.bam"
+			#print file_names_with_sex.pattern
 			date_and_batch = str(columns[1])
 			#take last 8 digits and assign to batch number
-			batch_number = int(date_and_batch[-8:])
-			if batch_number >= 1501266:
-				path_to_bam_directory = path_to_v5_bam + date_and_batch.replace('.', '-', 2) + '/assembly/'
-				rearrange_bam_location(path_to_bam_directory)
-		
-			elif batch_number < 1501266 and batch_number >= 1403083:
-				date_and_batch_universal = date_and_batch.strip('.-') 				
-				if date_and_batch_universal[5] == 1  and date_and_batch_universal[6] == 4:
-					path_to_bam_directory = path_to_v5_bam + '2014-' + date_and_batch_universal[3,4] + '-' + date_and_batch_universal[1,2] + '_' + str(batch_number) + '/assembly/'  #rearrange ddmmyy to 2014-mm-dd
-					rearrange_bam_location(path_to_bam_directory)
-					
-				elif date_and_batch_universal[5] == 1 and date_and_batch_universal[6] == 5:
-					path_to_bam_directory = path_to_v5_bam + '2015-' + date_and_batch_universal[3,4] + '-' + date_and_batch_universal[1,2] + '_' str(batch_number) + '/assembly/' # rearrange ddmmyy to 2015-mm-dd_nnnnnnnn
-					rearrange_bam_location(path_to_bam_directory)
-					
-			#the batch numbers 1403083(14), 1403237(14), 1403375(14), 1403633(14), 1500047, 1500335, 1501051 need rearranging in the format yyyy-mm-dd_nnnnnnnn #
-			#the following are exceptions 1404237  and 1404001 need the additional_path 2014-10-27_2014-11-12_rerun/assembly/
-			#1404418 needs additional_path 2014-11-12_2014-11-26_merged
-			#any batch with the date 170215 require the additional_path 2015-02-15_1500634 
+			batch_number = int(date_and_batch[-7:])
+			if batch_number >= 1501266 and batch_number != 1502972:
+				path_to_bam_directory = path_to_V5_bam + date_and_batch.replace('.', '-', 2) + '/assembly/'
+				for file in os.listdir(path_to_bam_directory):
+					if fnmatch.fnmatch(file, file_names_with_sex): 				
+						complete_path_to_bam = path_to_bam_directory + file
+						my_dict[sample_ID] = complete_path_to_bam
+			elif batch_number == 1502972:
+				path_to_bam_directory = path_to_V5_bam + '2015-08-11_1503151/assembly/'
+				for file in os.listdir(path_to_bam_directory):
+					if fnmatch.fnmatch(file, file_names_with_sex):
+						complete_path_to_bam = path_to_bam_directory + file
+						my_dict[sample_ID] = complete_path_to_bam			
+						print my_dict
 			
 
-== 1403083 or batch_number == 1403237 or batch_number ==  :
-				path_to_bam_directory = 
-				for file in os.listdir(path1_to_bam_directory):
-					
-					bam_pattern = sample_ID + '*.realigned.sorted.bam'
-					if fnmatch.fnmatch(file, bam_pattern):
-						complete_path_to_bam = path1_to_bam_directory + file
-						my_dict[sample_ID] = complete_path_to_bam 	
-				
-
-		
-#create a path to the directory using os.listdir
-
-	for file in os.listdir('path_to_directory'):
-		if fnmatch.fnmatch(file, 'pattern'):
-			complete_path_to_bam = path_to_P5__bam + additional_path + file			
-			my_dict[sample_ID] = complete_path_to_bam	
+#the batch numbers 1403083(14), 1403237(14), 1403375(14), 1403633(14), 1500047, 1500335, 1501051 need rearranging in the format yyyy-mm-dd_nnnnnnnn #		
+#			elif batch_number < 1501266 and batch_number >= 1403083:
+#				date_and_batch_universal = date_and_batch.strip('.-') 				
+#				if date_and_batch_universal[5] == 1  and date_and_batch_universal[6] == 4:
+#					path_to_bam_directory = path_to_v5_bam + '2014-' + date_and_batch_universal[3,4] + '-' + date_and_batch_universal[1,2] + '_' + str(batch_number) + '/assembly/'  #rearrange ddmmyy to 2014-mm-dd
+#					rearrange_bam_location(path_to_bam_directory)
+#					
+#				elif date_and_batch_universal[5] == 1 and date_and_batch_universal[6] == 5:
+#					path_to_bam_directory = path_to_v5_bam + '2015-' + date_and_batch_universal[3,4] + '-' + date_and_batch_universal[1,2] + '_' str(batch_number) + '/assembly/' # rearrange ddmmyy to 2015-mm-dd_nnnnnnnn
+#					rearrange_bam_location(path_to_bam_directory)
+#					
+#the following are exceptions 
+##1404237  and 1404001 need the additional_path 2014-10-27_2014-11-12_rerun/assembly/
+#			elif batch_number == 1404237 or batch_number == 1404001:
+#				path_to_bam_directory = path_to_v5_bam + '2014-10-27_2014-11-12_rerun/assembly/'
+#				rearrange_bam_location(path_to_bam_directory)
+#
+#			#1404418 needs additional_path 2014-11-12_2014-11-26_merged
+#			elif batch_number == 1404418:
+#				path_to_bam_directory = path_to_v5_bam + '2014-11-12_2014-11-26_merged/assembly/'
+#				rearrange_bam_location(path_to_bam_directory)
+#
+#			#any batch with the date 170215 require the additional_path 2015-02-15_1500634 
+#			elif batch_number >= 1500634 and batch_number <= 1500657:
+#				path_to_bam_directory = path_to_v5_bam + '2015-02-15_1500634/assembly/'
+#				rearrange_bam_location(path_to_bam_directory)			
+#
+	
 
 #loop through each dictionary entry and initiate Platypus calls on it
 
