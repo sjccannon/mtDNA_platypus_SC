c816d2a9d4a70a3dfa22002c2ce733b3585be921
Stuart Cannon <sc651@grace.ex.ac.uk>
2015-12-04 10:54:59 +0000

    tested obtaining the patient IDs from a file

--- platypus_revised.py
+++ platypus_revised.py
@@ -5,42 +5,52 @@ bam_path2 = "/mnt/Data4/targeted_sequencing"
 
 """program functionality"""
 
-def bam_finder(directory, patient_ID_list):
-	bam_locations = []
-	for root, dirs, files in os.walk(directory):
-		matches = fnmatch.filter(files, patter)
-		results.extend(os.path.join(base, f) for f in good files)
-	return results
+patient_ID_list = []
+patient_file = raw_input("please enter the file that contains the patient_IDs. It should be within the current directory and in a CSV format: ")
 
-def ID_extractor():
-	patient_file = raw_input("please enter the file that contains the patient_IDs. It should be within the current directory and in a CSV format.")
+def ID_extractor(patient_ID_list):
 	with open(patient_file, "r") as file:
 		for line in file:
+			columns = line.split()
+			patient_ID = columns[0]
+			patient_ID_list.append(patient_ID)
+ 	return patient_ID_list, patient_file
+
+
+#def bam_finder(directory, patient_ID_list):
+#	bam_locations = []
+#	for root, dirs, files in os.walk(directory):
+#		matches = fnmatch.filter(files, patter)
+#		results.extend(os.path.join(base, f) for f in good files)
+#	return results
+
 			
-			patient_ID =  	
-	return patient_ID_list
 
 """test functions"""
 
 #this fucntion should test that the quantity of patient IDs etracted is the same as in the source file.
 #it will also test the format of the extracted patient IDs is correct
 def ID_extractor_test(patient_ID_list):
-	assert (patient_ID_list == type(list)), "Input data is not a list format!"
+	assert type(patient_ID_list) is list, "Input data is not a list format!"
 	len_ID_list = len(patient_ID_list)
 	ID_format1 = re.compile('P5_\d?d{1,3}') #matches P5_a_bcd
-	ID_format2 = re.compile('[Pv]\d+_\d+') #matches v5_abc, v501_abc, P5_abc 
-	with open("mitochondrial_sensitivity_MODY.csv", "r") as file:
+	ID_format2 = re.compile('[Pv]\d+_\d+') #matches v5_abc, v501_abcd, P5_abc 
+	with open(patient_file, "r") as file:
 		num_lines = sum(1 for line in file)
-		assert (num_lines == len_ID_list), "not all patient_IDs  were extracted"
-	for element in patient_ID_list:
-		 assert (ID_format1.match(str(element)) or (ID_format2.match(str(element)), "new patient ID_detected"  
+		assert (num_lines == len(patient_ID_list)), "not all patient_IDs  were extracted"
+		for element in patient_ID_list:
+			element = str(element)
+			match1 = ID_format1.match(element)
+		        match2 = ID_format2.match(element)
+			assert (match1 or match2), "new patient_ID format detected"  
+	return patient_ID_list  
 
-def bam_finder_test():
+#def bam_finder_test():
 
 if __name__ == "__main__":
-	ID_extractor()
+	ID_extractor(patient_ID_list)
 	ID_extractor_test(patient_ID_list)
-	bam_finder(bam_path1, patient_ID_list)
-	bam_finder(bam_path_2, patient_ID_list)
+#	bam_finder(bam_path1, patient_ID_list)
+#	bam_finder(bam_path_2, patient_ID_list)
 
 	
