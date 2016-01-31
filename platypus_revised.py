03bdc3d5fa3b6812ae56fd2cf3b0239757e47ead
Stuart Cannon <sc651@grace.ex.ac.uk>
2016-01-31 14:24:24 +0000

    finalised exception capture

--- platypus_revised.py
+++ platypus_revised.py
@@ -1,4 +1,4 @@
-'''this program takes an input csv file taht must have tha patient ID in the first column. for the old versioning numbers it only searches the for bam files in the format /mnt/Data1/targeted_sequencing/P5_nnn-nnn/assembly/P5_nnn-nnn_nnn.realigned.sorted.bam. If you have earlier versions on your patient list then the code must be altered accordingly to identify the pahts to the files you need.'''
+''' this program takes an input csv file taht must have tha patient ID in the first column. for the old versioning numbers it only searches the for bam files in the format /mnt/Data1/targeted_sequencing/P5_nnn-nnn/assembly/P5_nnn-nnn_nnn.realigned.sorted.bam. If you have earlier versions on your patient list then the code must be altered accordingly to identify the pahts to the files you need.'''
 '''
 import a file containing patient IDs
 extract the IDs
@@ -11,36 +11,52 @@ run the list of bam files through Platypus variant caller
 import re, subprocess, os, fnmatch, shlex, os.path
 
 #global variables
+#the linux find command required to locate the bam files. This could become raw input rather than hard coded to increase functionality
 bam_path1 = "find /mnt/Data1/targeted_sequencing/ /mnt/Data4/targeted_sequencing/ -type f -name '*realigned*bam'"
-patients_in_input_file = re.compile('^\d+')
-ID_format1 = re.compile('P5_\d{1,2}_\d{1,3}') #matches P5_a_bcd
-ID_format2 = re.compile('[Pv]\d+_\d+') #matches v5_abc, v501_abcd, P5_abc
-patient_file = raw_input("please enter the file that contains the patient_IDs. It should be within the current directory and in a CSV format: ")
-patient_ID_list = [] #list of patient IDs extracted from an input file
-bam_locations_list = [] #list of bam file abolute paths obtained by the specified Linux 'find' command
-patient_bam_dict = {} #PatientId:BamFilePath
+#regular expression to match patient ID in format P5_a_bcd. Separated because legacy filenaming caused additional exceptions to the ID_format2 filenames.
+ID_format1 = re.compile('P5_\d{1,2}_\d{1,3}') 
+#regular expression to match patient Id in formats v5_abc, v501_abcd, P5_abc. 
+ID_format2 = re.compile('[Pv]\d+_\d+')
+#raw input where the user specifies the csv file to be analysed
+patient_file = raw_input("please enter the file that contains the patient_IDs in column[0]. It should be within the current directory and in a CSV format: ")
+#list of patient IDs to be appended by IDs extracted from an input file
+patient_ID_list = []
+#list of bam file abolute paths to be appended with output of the specified Linux 'find' command
+bam_locations_list = [] 
+#dictionary containing {key : value} as {Patient_ID: [bam_file_path]} 
+patient_bam_dict = {}
+#dictionary containing {key : value} as {Patient_ID:last four characters of patient ID}. Used as unique identifier and in combination wiht regular expression to avoid obtaining duplicate bam files for one patient.
 P5_dict_from_patient_list = {} #ID:last four characters of patient_ID
+#Platypus variant caller to be used on each obtained patient bam file
 path_to_platypus = "/usr/share/platypus/Platypus_0.8.1/Platypus.py" 
+#GRC build 37 reference genome required for platypus
 build_37_ref = "/mnt/Data1/resources/human_g1k_v37.fasta"
+#file path for the output of platypus
 base_path = "/mnt/Data4/working_directory/stuart/python-2-7-10/scripts/platypus/variants/"
-
 
 """program functionality"""
 
 '''
-Function that uses Linux OS to generate a list of all realigned bam files in target sequencing. 
+Function that uses Linux OS to generate a list of all realigned bam files in the targeted_sequencing directory. 
 Identifies everyfile on the server ending in '*realigned*bam' and stores them in a list
 removes test files or reruns
 '''
-
 def bam_list(bam_find1, bam_locations_list):
-	print "aggregating bam file paths in target sequencing"
+	#inform the user of processing stage
+	print "Aggregating bam file paths in targeted_sequencing directory"
+	#take the input find command bam_find1 string and split it into separate arguments
 	args = shlex.split(bam_find1)
+	#using the os to call the arguments and pipe the output. prevents need to write another file
 	bam_1 = subprocess.Popen(args, stdout=subprocess.PIPE)
+	#capture the piped output as a tuple ([bamfiles], stderrNULL)
 	stdout = bam_1.communicate()
+	#removes white space and splits by the newline character
 	bam_locations_local = stdout[0].rstrip().split('\n')
+	#if statement ensures the bam_locations_list is only appended once. Enables the function to be called by the test function 
 	if len(bam_locations_list) < len(patient_ID_list):
+		#iterates through the bam location strings in the bam_locations_local list
 		for element in bam_locations_local:
+			#appends bam_locations_list only if 'rerun' is not in the name effectively removign filepaths the any rerun samples
 			if 'rerun' not in element:
 				bam_locations_list.append(element)
 	print "file paths aggregated"
@@ -51,8 +67,9 @@ Fucntion to extract patient sample IDs from the first column of a csv file
 '''
 
 def ID_extractor(patient_ID_list, patient_file):
-	print "extracting patient IDs from specified input file"
+	print "Extracting patient IDs from specified input file. please ensure the patient ID is in the first column of the file"
 	with open(patient_file, "r") as file:
+		#for loop that counts the number of lines in the file
 		for i, line in enumerate(file):
 			#this loop allows the test function to call this function every time. otherwise the patient IDs are re-appended to the list. +1 because the count starts at 0.
 			if len(patient_ID_list) < i+1:
@@ -60,7 +77,7 @@ def ID_extractor(patient_ID_list, patient_file):
 				patient_ID = columns[0]
 				patient_ID_list.append(patient_ID)
 				assert (patient_ID in patient_ID_list)
-	print "Patient ID extraction complete. ' + str(len(patient_ID_list)) + ' patient IDs extracted'
+	print 'Patient ID extraction complete. ' + str(len(patient_ID_list)) + ' patient IDs extracted'
 	return patient_ID_list
 '''
 Main method - identify the paths to the required bam files from the list of patient IDs and all of the bam files in the target directories.
@@ -72,25 +89,34 @@ P5 samples 385 -  on Data1 are only included for 'run1'
 
 #function to only identify the bam files needed form the patient ID list
 def patient_bam_extractor(patient_ID_list, bam_locations_list):
-	print 'Locating bam file paths for patient IDs in patient ID list and storing in a dictionary'
+	print 'Locating bam file paths for patient IDs in patient_ID_list and storing in a dictionary'
 	#iterate through patient_ID_list
 	for patient_ID in patient_ID_list:
 		#generate a list of raw matches
 		ID_raw_match = [location for location in bam_locations_list if patient_ID in location]
+		#this statement prevents the list being appended multiple times
 		if ID_raw_match != []:
 			#append the raw match to the patientID it is associated with
 			patient_bam_dict[patient_ID] = ID_raw_match
-			print patient_ID + ' added.' + 'Dictionary length = ' + str(len(patient_bam_dict.keys())  
+			print patient_ID + ' added.' + 'Dictionary length = ' + str(len(patient_bam_dict.keys())
+		#string matching to extract the numerics of the patient_ID
 		if ID_format1.match(patient_ID):
+			#extract the ID_numbers
 			match_1_ID_num = patient_ID[-4:]
+			#generate a dictionary of P5 patients
 			P5_dict_from_patient_list[patient_ID] = match_1_ID_num
 			#print 'p5_match dict' + str(len(P5_dict_from_patient_list.keys()))
+	#iterates through the patient IDs and ID numbers (numerics) in the p5 dictionary
 	for patient_ID, ID_num in P5_dict_from_patient_list.iteritems():
+		#this is a generic random expresion compiled to match the majority of samples, notice the interchangeable ID_numbers
 		bam_match_format = re.compile('.*Data1.*P5_\d{3}-\d{3}/assembly.*P5_\d{3}-\d{3}.*(%s)\.realigned.sorted.bam'%ID_num)
+		#this is to account for exceptions where there is a 'run1' in the filepath 
 		bam_match_run1_format = re.compile('.*Data1.*P5_\d{3}-\d{3}_run1/assembly.*P5_\d{3}-\d{3}.*(%s)\.realigned.sorted.bam'%ID_num)
+		#nested loop to compare each match to the bam_location
 		for bam_location in bam_locations_list:
 			p5_match = re.search(bam_match_format, bam_location)
 			p5_match2 = re.search(bam_match_run1_format, bam_location)
+			#the following two if statements update 
 			if p5_match and patient_ID not in patient_bam_dict.keys():
 				patient_bam_dict[patient_ID] = [bam_location]
 				print patient_ID + ' added.' + 'Dictionary length = ' + str(len(patient_bam_dict.keys())
@@ -98,11 +124,13 @@ def patient_bam_extractor(patient_ID_list, bam_locations_list):
 				patient_bam_dict[patient_ID] = [bam_location]
 				print patient_ID + ' added.' + 'Dictionary length = ' + str(len(patient_bam_dict.keys())
 	for patient_ID, bam_path in patient_bam_dict.iteritems():
+		#removes the items with 'test' in the string
 		if len(bam_path) > 1:
                         for item in bam_path:
                                 if 'test' in item:
                                         bam_path.remove(item)
-					print 'Removed test bam file: ' + item + '. ' + 'Patient ' + patient_ID + ' still has a non-test bam file to be analysed.'
+                                        #exception capture notification
+                                        print 'Removed test bam file: ' + item + '. ' + 'Patient ' + patient_ID + ' still has a non-test bam file to be analysed.'
                                         patient_bam_dict[patient_ID] = [bam_path]
 	print 'Dictionary complete. ' + str(len(patient_bam_dict.keys()) + ' patient bam files located.'
 
@@ -124,7 +152,8 @@ def platypus_caller(path_to_platypus, build_37_ref, base_path, patient_bam_dict)
 Fucntion to filter vcf files for mitochondrial mutation 3243G>A
 and delete original vcf files once formatted
 '''
-
+with open ("filtered_platypus_varaints", "a") as out_file:
+	list_of_files = os.listdir(vcf_location)
 
 '''
 Test functions and exception capture
@@ -207,16 +236,15 @@ def test_platypus_output(path_to_platypus, build_37_ref, base_path, patient_bam_
 check the filtered vcf file
 '''
 #call the variant filter funtion
+
 #check the output file exists 
 #check the 
-
-
- 
 
 if __name__ == "__main__":
 	ID_extractor_test(patient_ID_list, patient_file)
 	bam_list_test(bam_path1, bam_locations_list)
 	patient_bam_extractor_test(patient_ID_list, bam_locations_list)
+	test_platypus_output(path_to_platypus, build_37_ref, base_path, patient_bam_dict)
 
 #	for key, value in patient_bam_dict.iteritems():
 #		print str(key) + str(value)
--- variant_filtering.py
+++ variant_filtering.py
@@ -1,6 +1,8 @@
-import os, os.path, itertools
+import os, os.path, itertools, re
 
 vcf_location = "/mnt/Data4/working_directory/stuart/python-2-7-10/scripts/platypus/variants/"
+
+re_ID = re.compile("platypus_m\.3243A")
 
 with open("m.3243A>G_filtered", "a") as out_file:
 	list_of_files = os.listdir(vcf_location)
@@ -11,7 +13,11 @@ with open("m.3243A>G_filtered", "a") as out_file:
 
 for filename in os.listdir(vcf_location):
 	with open(vcf_location + filename, "r") as file, open("m.3243A>G_filtered", "a") as out_file:
-		sample_ID = filename.strip('platypus_m.3243A')
+		filename = str(filename)
+		sample_ID = re.sub(re_ID, "", filename)
+#		sample_ID = re_ID.sub filename.strip("platypus_m.3243A")
+		print filename
+		print sample_ID
 		for line in file:
 			if line.startswith("MT"):
 #				print line
