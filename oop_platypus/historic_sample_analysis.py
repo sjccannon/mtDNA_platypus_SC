52be55d7aad04a962ac02c64337d4563cfb2a860
Stuart Cannon <sc651@grace.ex.ac.uk>
2016-05-26 13:18:12 +0100

    working on find

--- oop_platypus/historic_sample_analysis.py
+++ oop_platypus/historic_sample_analysis.py
@@ -1,3 +1,4 @@
+
 '''the following code enables historic samples to be analysed with platypus variant caller although additional functionality 
 could be added to the clases'''
 
@@ -6,7 +7,7 @@ could be added to the clases'''
 samples are read into a 1D array although additional methods could be constructed
 where more data fields are included in the input data'''
 
-import re
+import re, subprocess, shlex, glob
 
 class Input_data:
 
@@ -14,6 +15,7 @@ class Input_data:
             self.samples_file = samples_file
             self.patient_id_array = []
 	    self.non_recognised_ids = []
+	    self.sample_instances = []
 
         #extract the first column of the data into an class array
         def patient_id_to_list(self):
@@ -43,26 +45,82 @@ class Input_data:
 		print 'They have also been written to non_recognised_ids.txt'
 		with open('non_recognised_ids.txt', "w") as out_file:
 		    for item in self.non_recognised_ids:
-			outfile.write(item + '\n')
+			out_file.write(item + '\n')
 		return self.non_recognised_ids
 	    else:
    	        print 'all patient IDs are in the recognised format'
-
-
-	'''    	
+	    	
         def samples(self):
-            sample_insatnces = []
             for item in self.patient_id_array:
-                sample_instances.append(Samples(item))
-            return sample_instances
-	'''
+                self.sample_instances.append(Sample(item))
+            return self.sample_instances
+
+class Sample:
+        base_path = "/mnt/Data1/targeted_sequencing"
+        #initialise
+        def __init__(self, sample_id):
+            self.sample_id = str(sample_id)
+	    self.data = ""
+            self.bam_array = []
+
+        def sample_sorter(self):
+            if self.sample_id.startswith('P'):
+                self.data = 'data1'
+            elif self.sample_id.startswith('v'):
+                self.data = 'data4'
+            else:
+                self.data = 'not_P_or_v'
+            return self.data
+
+	def find_function(self, cmd_array):
+	    found = subprocess.Popen(cmd_array, stdout=subprocess.PIPE)
+	    output, err = found.communicate()
+	    print output, err
+		
+        def locate_bam_file(self):
+            if self.data == 'data1':
+                sliced_id = self.sample_id[-3:]
+		print sliced_id
+                sample_regex = "P5_*-*_%s*realigned*bam" %sliced_id
+                arg = glob.glob(self.base_path + "/P5*/assembly")
+		for item in arg:
+		    cmd = "find " + item + " -name " + sample_regex + " -type f"
+		    cmd_array = shlex.split(cmd)
+		    self.find_function(cmd_array)
+	
+#cmd = 'sudo rm -rf'
+#basearg = shlex.split(cmd)
+#arg = basearg + glob.glob(path+"/*"
+	
+                    #use subprocess.Popen to capture stdout and pipe to found_bam_array
+                    #if len(found_bam_array) > 0:
+                    #    print 'multiple bam files identified for patient_id' + self.sample_ID
+                    #    for idx, item in enumerate(found_bam_array):
+                    #            print str(idx) + " " + str(item)
+                    #    user_input_statemet = "Enter the index of the bam file you wish to analyse %s : " %self.sample_id
+                    #    user_input = raw_input(user_input_statement)
+                    #    self.bam_file_array.append(found_bam_array[int(raw_input)])
+                    #else:
+                    #    print 'appending %s to bam_file_array for sample %s' %(str(found_bam_array[0], self.sample_id)
+                    #    self.bam_file_array.append(found_bam_array[0])
+                    #return self.bam_file_array
+                #elif self.bam_file_array[1] == 'data4':
+                 #   found_bam_array = []
+
 
 
 if __name__ == "__main__":
 	pd = Input_data('/mnt/Data4/working_directory/stuart/python-2-7-10/scripts/platypus/obsolete/mitochondrial_sensitivity_MODY.csv')
-	id_array = pd.patient_id_to_list()
-	print len(id_array)
+	pd.patient_id_to_list()
 	pd.check_id_format()
+	samples = pd.samples()
+	for sample in samples:
+		sample.sample_sorter()
+		print str(sample.sample_id) + ' ' + str(sample.data)
+		sample.locate_bam_file()
+
+
+
 '''
 wrapper_script
 from bam_finder.py import *
@@ -78,12 +136,6 @@ platypus_output_path takes the bam_file_iput_path and creates an output path for
 
 
 class Sample(object):
-	base_path = "/mnt/Data4/targeted_sequencing"
-
-	#initialise
-        def __init__(self, sample_ID):
-            self.sample_ID = sample_ID
-            self.bam_file_array = [sample_ID]
 
 	#class methods
         #bam_file_input_path appended to bam file array with sample_ID
